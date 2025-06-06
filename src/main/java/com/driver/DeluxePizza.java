1f92b1771ab7d40bae16737e92ff8859ecc426bf
hemanthbommana <hemanthbommana2001@gmail.com>
2025-06-06 17:21:11 +0530

    1st commit

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -2,8 +2,16 @@ package com.driver;
 
 public class DeluxePizza extends Pizza {
 
+    
+
     public DeluxePizza(Boolean isVeg) {
         super(isVeg);
+        this.isDeluxe = true;
+        setPrice(400);
+        setBill("Base Price Of The Pizza: 400\n");
+        super.addExtraCheese();
+        super.addExtraToppings();
         // your code goes here
+
     }
 }
--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -5,30 +5,59 @@ public class Pizza {
     private int price;
     private Boolean isVeg;
     private String bill;
+    protected boolean isDeluxe;
+
+    private boolean cheeseAdded = false;
+    private boolean toppingsAdded = false;
+    private boolean takeawayAdded = false;
 
     public Pizza(Boolean isVeg){
+        
         this.isVeg = isVeg;
+        this.price = 300;
+        this.bill = "Base Price Of The Pizza: 300 \n";
         // your code goes here
     }
-
     public int getPrice(){
         return this.price;
     }
 
     public void addExtraCheese(){
         // your code goes here
+        if (!cheeseAdded) {
+            this.price += 80;
+            this.bill += "Extra Cheese Added: 80\n";
+            cheeseAdded = true;
+        }
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if (!toppingsAdded) {
+            this.price += 70;
+            this.bill += "Extra Toppings Added: 70\n";
+            toppingsAdded = true;
+        }
     }
 
     public void addTakeaway(){
         // your code goes here
+        if (!takeawayAdded) {
+            this.price += 20;
+            this.bill += "Paperbag Added: 20 \n";
+            takeawayAdded = true;
+        }
     }
+    public void setPrice(int price){
+        this.price = price;
 
+    }
+    public void setBill(String bill){
+        this.bill = bill;
+    }
     public String getBill(){
         // your code goes here
-        return this.bill;
+        
+        return this.bill+ "Total Price: "+this.price;
     }
 }
